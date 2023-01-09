a3ffd83ada9060dad6840d82d18ce66338d00b44
wsydn <wsydney@web.de>
2023-01-09 11:37:46 +0100

    craft make plugin

---
+++ src/Plugin.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace wsydney76\members;
+
+use Craft;
+use craft\base\Model;
+use craft\base\Plugin as BasePlugin;
+use wsydney76\members\models\Settings;
+
+/**
+ * Members plugin
+ *
+ * @method static Plugin getInstance()
+ * @method Settings getSettings()
+ * @author wsydney76 <wsydney@web.de>
+ * @copyright wsydney76
+ * @license MIT
+ */
+class Plugin extends BasePlugin
+{
+    public string $schemaVersion = '1.0.0';
+    public bool $hasCpSettings = true;
+
+    public static function config(): array
+    {
+        return [
+            'components' => [
+                // Define component configs here...
+            ],
+        ];
+    }
+
+    public function init()
+    {
+        parent::init();
+
+        // Defer most setup tasks until Craft is fully initialized
+        Craft::$app->onInit(function() {
+            $this->attachEventHandlers();
+            // ...
+        });
+    }
+
+    protected function createSettingsModel(): ?Model
+    {
+        return Craft::createObject(Settings::class);
+    }
+
+    protected function settingsHtml(): ?string
+    {
+        return Craft::$app->view->renderTemplate('members/_settings.twig', [
+            'plugin' => $this,
+            'settings' => $this->getSettings(),
+        ]);
+    }
+
+    private function attachEventHandlers(): void
+    {
+        // Register event handlers here ...
+        // (see https://craftcms.com/docs/4.x/extend/events.html to get started)
+    }
+}
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace wsydney76\members\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Members settings
+ */
+class Settings extends Model
+{
+}
